f251b84d96f70c20b51036d7b696cbbd02195703
Daryl <daryl@globalconnect.com>
2024-08-28 12:53:17 +0800

    removed hardcoded string

--- app/src/main/java/com/daryl/mob23quizapp/data/models/Validator.kt
+++ app/src/main/java/com/daryl/mob23quizapp/data/models/Validator.kt
@@ -1,5 +1,8 @@
 package com.daryl.mob23quizapp.data.models
 
+import com.daryl.mob23quizapp.core.Constants.PASSWORD
+import com.daryl.mob23quizapp.core.Constants.PASSWORD_MISMATCH
+
 data class Validator(
     val value: String,
     val regExp: String,
@@ -9,11 +12,11 @@ data class Validator(
         fun validate(vararg fields: Validator): String? {
             fields.forEach { field ->
                 val value = field.value
-                if(field.errorMessage.contains("Password")) {
+                if(field.errorMessage.contains(PASSWORD)) {
                     val midIdx = value.length / 2
                     val password = value.substring(0, midIdx)
                     val password2 = value.substring(midIdx)
-                    if(password != password2) return "Both passwords must match!"
+                    if(password != password2) return PASSWORD_MISMATCH
                 }
                 if(!Regex(field.regExp).matches(value)) return field.errorMessage
             }
